f1c6eed106956cf3e018141016bd615ab28e23df
Carl Douglas <105003+carld@users.noreply.github.com>
2017-12-21 15:35:25 +1300

    test a shorter path

--- spec/hyper_navigator_spec.rb
+++ spec/hyper_navigator_spec.rb
@@ -107,5 +107,18 @@ RSpec.describe HyperNavigator do
       expect(result).to include("/a/b/c/d")
       expect(result).not_to include("/a", "/a/b", "/a/b1", "/a/b/c")
     end
+
+    it "returns just the leaf documents fetched for the given short path" do
+      allow(HyperNavigator).to receive(:get).with('/', {}).and_return(double(body: doc_root))
+      allow(HyperNavigator).to receive(:get).with('/a', {}).and_return(double(body: doc_a))
+      allow(HyperNavigator).to receive(:get).with('/a/b', {}).and_return(double(body: doc_a_b))
+      allow(HyperNavigator).to receive(:get).with('/a/b1', {}).and_return(double(body: doc_a_b1))
+      allow(HyperNavigator).to receive(:get).with('/a/b/c', {}).and_return(double(body: doc_a_b_c))
+      allow(HyperNavigator).to receive(:get).with('/a/b/c/d', {}).and_return(double(body: doc_a_b_c_d))
+
+      result = HyperNavigator.surf_to_leaves('/', ["a", "b"]).map {|x| x.href }
+      expect(result).to include("/a/b")
+      expect(result).not_to include("/a", "/a/b1", "/a/b/c")
+    end
   end
 end
